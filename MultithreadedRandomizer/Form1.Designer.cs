995b0cb26887136b6763cf04020ecd15fd17d815
Colibri <stucinskasdomantas@gmai.com>
2018-01-07 20:32:32 +0200

    Multithreaded randomizer

---
+++ MultithreadedRandomizer/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace MultithreadedRandomizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.threadsAmount = new System.Windows.Forms.TextBox();
+            this.validator = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.start = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Threads";
+            // 
+            // threadsAmount
+            // 
+            this.threadsAmount.Location = new System.Drawing.Point(64, 26);
+            this.threadsAmount.Name = "threadsAmount";
+            this.threadsAmount.Size = new System.Drawing.Size(100, 20);
+            this.threadsAmount.TabIndex = 1;
+            this.threadsAmount.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // validator
+            // 
+            this.validator.AutoSize = true;
+            this.validator.Location = new System.Drawing.Point(171, 29);
+            this.validator.Name = "validator";
+            this.validator.Size = new System.Drawing.Size(0, 13);
+            this.validator.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(15, 82);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(560, 84);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Thread ID";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Random string";
+            this.columnHeader2.Width = 594;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 173);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(560, 126);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(15, 52);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 6;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(114, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 311);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.validator);
+            this.Controls.Add(this.threadsAmount);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox threadsAmount;
+        private System.Windows.Forms.Label validator;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MultithreadedRandomizer/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+
+namespace MultithreadedRandomizer
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection connection = new OleDbConnection();
+        const string allowedChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#@$^*()";
+        static Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\Colibri\source\repos\MultithreadedRandomizer\MultithreadedRandomizer\App_data\randomStrings.mdb";
+                connection.Open();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error connecting db: " + ex);
+            }
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int treadsValue;
+            if (!string.IsNullOrEmpty(threadsAmount.Text))
+            {
+                if (!int.TryParse(threadsAmount.Text, out treadsValue))
+                {
+                    validator.Text = "Value must be a number!";
+                }
+                else
+                {
+                    if (treadsValue < 2)
+                        validator.Text = "Value must be more than 2!";
+                    else if (treadsValue > 15)
+                        validator.Text = "Value must be less than 16!";
+                    else
+                        validator.Text = "";
+                }
+            }
+            else
+            {
+                validator.Text = "";
+            }
+        }
+
+        
+
+
+   
+        
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+                OleDbCommand command = new OleDbCommand();
+                command.Connection = connection;
+                command.CommandText = "INSERT INTO randomStrings (Timed) VALUES ('" + DateTime.Now.ToString("MM/dd/yyyy") + "')";
+
+                command.ExecuteNonQuery();
+                MessageBox.Show("saved");
+                connection.Close();
+            
+
+
+
+            /*
+            OleDbCommand command = new OleDbCommand();
+            command.Connection = connection;
+            command.CommandText = "INSERT INTO randomStrings (ThreadID, Time, Date) VALUES ('@ThreadID, @Time, @Date)";
+            connection.Open();
+
+            //int test = Int32.Parse(threadsAmount.Text);
+            //DateTime dabar = DateTime.Now;
+            //command.CommandText = "insert into randomStrings (ThreadID,Time,Date) values('1,1/11/2017,basd')";
+            // command.CommandText = "insert into randomStrings (TreadID,Time,Date) values('" + test +"','" + dabar +"','" + "any text" + "')";
+
+            command.Parameters.AddWithValue("@ThreadID", "1");
+            command.Parameters.AddWithValue("@Time", "1/11/2017");
+            command.Parameters.AddWithValue("@Date", "basd");
+
+
+            int yy = command.ExecuteNonQuery();
+            MessageBox.Show("Data saved");
+            connection.Close();
+            */
+            /*
+            command.CommandText = "select * from randomStrings where ID=1";
+            OleDbDataReader reader = command.ExecuteReader();
+            if (reader.Read())
+                MessageBox.Show("parameter exists");
+            else
+                MessageBox.Show("No such parameter");
+
+            */
+            //connection.Close();
+            /*
+           int id = 0;
+           DateTime dateNow = DateTime.Now;
+
+           ListViewItem item = new ListViewItem(id.ToString());
+           item.SubItems.Add(generateRandomString());
+           listView1.Items.Add(item);
+           */
+        }
+
+        /// Functions
+        internal static string generateRandomString()
+        {
+            int strLenght = rnd.Next(6, 16);
+            char[] chars = new char[strLenght];
+            for (int i = 0; i < strLenght; i++)
+                chars[i] = allowedChars[rnd.Next(0, allowedChars.Length)];
+
+            return new string(chars);
+        }
+    }
+}
